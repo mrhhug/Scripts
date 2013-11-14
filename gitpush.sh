2b51e7246fe0aa6246ebf71738a8ed333654da32
mrhhug@gmail.com <mrhhug@gmail.com>
2013-11-14 01:03:47 -0500

    commit from script

--- gitpush.sh
+++ gitpush.sh
@@ -7,11 +7,12 @@ do
 	git push origin master
 	echo 'End '$i
 done
-cd ~/Scripts
-git add -A
-git commit -m 'commit from script'
-git push origin master
-cd /srv/http
-git add -A
-git commit -m 'commit from script'
-git push origin master
+for i in {$HOME/Scripts,'/srv/http'}
+do
+	cd $i
+	echo 'Start '$i
+	git add -A
+	git commit -m 'commit from script'
+	git push origin master
+	echo 'End '$i
+done
