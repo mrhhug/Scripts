c5fda85ec58b2af88b14c767e3f9351d9ded22ec
mrhhug@gmail.com <mrhhug@gmail.com>
2013-11-14 00:52:21 -0500

    commit from script

--- gitpush.sh
+++ gitpush.sh
@@ -1,19 +1,12 @@
-cd ~/School/CSCI/ACM-ICPC
-git add -A
-git commit -m 'commit from script'
-git push origin master
-cd ~/School/CSCI/CS4520
-git add -A
-git commit -m 'commit from script'
-git push origin master
-cd ~/School/CSCI/CS3530
-git add -A
-git commit -m 'commit from script'
-git push origin master
-cd ~/School/CSCI/CS3310
-git add -A
-git commit -m 'commit from script'
-git push origin master
+for i in 'ACM-ICPC' 'CS4520' 'CS3530' 'CS3310' 
+do
+	cd ~/School/CSCI/$i
+	echo 'Start $i'
+	git add -A
+	git commit -m 'commit from script'
+	git push origin master
+	echo 'End $i'
+done
 cd ~/Scripts
 git add -A
 git commit -m 'commit from script'
