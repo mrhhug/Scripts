520608823c8ac450c1abe055999bef5ccb60fedb
mrhhug@gmail.com <mrhhug@gmail.com>
2014-02-21 23:49:55 -0500

    commit from script

--- gitpull.sh
+++ gitpull.sh
@@ -6,7 +6,7 @@ cd ~/School/CSCI/CS4150
 git pull origin master
 cd ~/School/CSCI/CS3610
 git pull origin master
-cd ~/School/CSCI/ACM-ICPC
+cd ~/School/Clubs/ACM/CommonsMenuApp
 git pull origin master
 cd ~/Scripts
 git pull origin master
--- gitpush.sh
+++ gitpush.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {'ACM-ICPC','CS3540','CS4150','CS3610','CS3510'}
+for i in {'CS3540','CS4150','CS3610','CS3510'}
 do
 	cd $HOME/School/CSCI/$i
 	echo 'Start '$i
@@ -8,6 +8,12 @@ do
 	git push origin master
 	echo 'End '$i
 done
+cd ~/School/Clubs/ACM/CommonsMenuApp
+echo 'Start CommonsMenuApp'
+git add -A
+git commit -m 'commit from script'
+git push origin master
+echo 'End CommonsMenuApp'
 for i in {$HOME/Scripts,'/srv/http'}
 do
 	cd $i
