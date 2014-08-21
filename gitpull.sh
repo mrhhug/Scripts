ae08e2e2c1e168307f37a11ac924491583019928
mrhhug <mrhhug@gmail.com>
2014-08-21 16:11:45 -0400

    commit from script

--- gitpull.sh
+++ gitpull.sh
@@ -1,5 +1,7 @@
 cd ~/School/MATH/MATH\ 3332/
 git pull origin master
+cd ~/School/CSCI/CS4500/
+git pull origin master
 cd ~/Scripts
 git pull origin master
 #cd /srv/http
--- gitpush.sh
+++ gitpush.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-for i in {'MATH\ 3332'}
+for i in {'CS4500'}
 do
-	cd $HOME/School/MATH/$i
+	cd $HOME/School/CSCI/$i
 	echo 'Start '$i
 	git add -A
 	git commit -m 'commit from script'
 	git push origin master
 	echo 'End '$i
 done
-for i in {$HOME/Scripts} #,'/srv/http'}
+for i in {$HOME/Scripts,'$HOME/School/MATH/MATH\ 3332'} #,'/srv/http'}
 do
 	cd $i
 	echo 'Start '$i
