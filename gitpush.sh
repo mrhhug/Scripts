9ea4b733bc3d9ea809a358e0fe791e836469eaac
mrhhug <mrhhug@gmail.com>
2014-08-21 16:18:39 -0400

    commit from script

--- gitpush.sh
+++ gitpush.sh
@@ -8,9 +8,20 @@ do
 	git push origin master
 	echo 'End '$i
 done
-for i in {$HOME/Scripts,$HOME'/School/MATH/MATH\ 3332'} #,'/srv/http'}
+
+for i in {$HOME/Scripts} #,'/srv/http'}
 do
 	cd $i
+	echo 'Start '$i
+	git add -A
+	git commit -m 'commit from script'
+	git push origin master
+	echo 'End '$i
+done
+
+for i in {'MATH 3332'} #,'/srv/http'}
+do
+	cd $HOME/School/MATH/$i
 	echo 'Start '$i
 	git add -A
 	git commit -m 'commit from script'
